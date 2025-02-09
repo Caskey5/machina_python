bd416d58a24d403d429900d3adabc58748231bed
Antonio Šiljac <siljacantonio@gmail.com>
2025-02-09 16:48:29 +0100

    Delete Zadatak - 1 - P1.py

--- Zadatak - 1 - P1.py
+++
@@ -1,8 +0,0 @@
-broj = int(input("Unesite broj učenika: "))
-veličina = int(input ("Unesite veličinu busa: "))
-
-bus = (broj + veličina - 1) // veličina
-
-print ("Potrebno je unajmiti", bus, "busa!")
-
-
